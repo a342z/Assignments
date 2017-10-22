b13c1dffb05914dd35f0da52c6abd5bd0290f78d
Abdulrahman Zaki <abdulrahman.zaki.342@gmail.com>
2017-10-22 23:20:15 +0200

    Add files via upload

---
+++ ValueOfBit/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValueOfBit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number , p;
+            string binary;
+
+            Console.WriteLine("Enter a number :");
+            number = int.Parse(Console.ReadLine());
+
+            binary = Convert.ToString(number, 2);
+            Console.WriteLine("The Number in Binary :\n" + binary );
+
+            Console.WriteLine("Enter a specific Position P :");
+            p = int.Parse(Console.ReadLine());
+            Console.WriteLine("The value of Bit at Position  " + p + " is:\n" + binary[p - 1]);
+            Console.ReadLine();
+        }
+    }
+}
