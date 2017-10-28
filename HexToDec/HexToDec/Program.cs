462dbd3c61f379fe44bc95b94749363f2186632f
Abdulrahman Zaki <abdulrahman.zaki.342@gmail.com>
2017-10-28 23:14:49 +0200

    Add files via upload

---
+++ HexToDec/HexToDec/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HexToDec
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string hex;
+            Console.WriteLine("Enter a Hexa Number : ");
+            hex = Console.ReadLine();
+
+            int dec = Convert.ToInt32(hex, 16);
+            Console.WriteLine("The number in decimal =  " + dec);
+
+            Console.ReadKey();
+
+        }
+    }
+}
